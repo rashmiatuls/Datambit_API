34bb6faaeba525ad8a28ee265837aa942ac2b247
rashmiatuls <rashmiatuls@gmail.com>
2024-06-18 19:26:33 +0530

    adding docker file

--- app/config.py
+++ app/config.py
@@ -4,5 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    MAX_CONTENT_LENGTH = int(os.getenv('MAX_CONTENT_LENGTH', 200 * 1024 * 1024))  # 200 MB by default
+    try:
+        max_content_length_str = os.getenv('MAX_CONTENT_LENGTH', str(200 * 1024 * 1024))  # 200 MB by default as string
+        MAX_CONTENT_LENGTH = int(max_content_length_str)
+    except ValueError:
+        MAX_CONTENT_LENGTH = 200 * 1024 * 1024  # Fallback to default value
+        print("Warning: Invalid MAX_CONTENT_LENGTH value. Using default 200 MB.")
+
     UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', 'app/uploads')
--- run.py
+++ run.py
@@ -1,21 +1,23 @@
+import os
 from app import create_app
 import logging
-import os
+
+# Create the Flask application
+app = create_app()
+
+# Ensure the upload folder exists
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+# Set up logging
+logging.basicConfig(filename='app.log', level=logging.INFO,
+                    format='%(asctime)s %(levelname)s: %(message)s')
+
+app.logger.info("Starting Flask server...")
 
 if __name__ == '__main__':
-    os.environ['FLASK_APP'] = 'app:create_app'  # Set the FLASK_APP environment variable
-    os.environ['FLASK_ENV'] = 'development'  # Set the FLASK_ENV environment variable to development mode
-
-    # Run the Flask application using flask run with --reload option
-    os.system('flask run --reload')
-
-    # Ensure the upload folder exists
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
-
-    # Set up logging
-    logging.basicConfig(filename='app.log', level=logging.INFO, 
-                        format='%(asctime)s %(levelname)s: %(message)s')
-
-    app.logger.info("Starting Flask server...")
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    # Set environment variables for Flask development mode
+    os.environ['FLASK_ENV'] = 'development'  # This ensures the server restarts on code changes
+    
+    # Run the Flask application in debug mode to enable auto-reloading
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)), debug=True)
